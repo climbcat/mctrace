38dffa91dd3e87ca69832e6ba64d1914455e10c6
Jakob Garde <jakob@rigsters.com>
2025-08-15 11:11:33 +0200

    mark the transforms in 3D as generated by the instrument config

--- main.cpp
+++ main.cpp
@@ -15,6 +15,49 @@
 #include "src/PSI_DMC_config.h"
 
 
+void RunDisplayLoop(Array<Component*> comps) {
+
+    CbuiInit("mctrace", false);
+
+    Perspective persp = ProjectionInit(cbui.plf.width, cbui.plf.height);
+    OrbitCamera cam = OrbitCameraInit(persp.aspect);
+    Array<Wireframe> objs = InitArray<Wireframe>(cbui.ctx->a_pers, 100);
+
+    Wireframe box = CreateAABox(0.2f, 0.2f, 0.2f);
+
+    Wireframe plane = CreatePlane(10);
+    objs.Add(plane);
+
+    while (cbui.running) {
+        CbuiFrameStart();
+        OrbitCameraUpdate(&cam, cbui.plf.cursorpos.dx, cbui.plf.cursorpos.dy, cbui.plf.left.ended_down, cbui.plf.scroll.yoffset_acc);
+        OrbitCameraPan(&cam, persp.fov, persp.aspect, cbui.plf.cursorpos.x_frac, cbui.plf.cursorpos.y_frac, MouseRight().pushed, MouseRight().released);
+        // start
+
+        objs.len = 1;
+
+        // (re-) generate the world matrices
+        SceneGraphUpdate();
+
+        for (s32 i = 0; i < comps.len; ++i) {
+            Component *comp = comps.arr[i];
+
+            Wireframe marker = box;
+            marker.color = COLOR_BLUE;
+            marker.transform = comp->transform->t_world;
+            objs.Add(marker);
+        }
+
+        // end 
+        Array<Vector3f> segments = WireframeLineSegments(cbui.ctx->a_tmp, objs);
+        RenderLineSegmentList(cbui.image_buffer, cam.view, persp.proj, cbui.plf.width, cbui.plf.height, objs, segments);
+
+        CbuiFrameEnd();
+    }
+    CbuiExit();
+}
+
+
 void RunProgram() {
     TimeFunction;
 
@@ -27,9 +70,10 @@ void RunProgram() {
     // config for the particular instrument, PSI_DMC
     PSI_DMC spec = {};
     Init_PSI_DMC(&spec);
-    Config_PSI_DMC(ctx->a_life, &spec, &instr);
+    Array<Component*> comps = Config_PSI_DMC(ctx->a_life, &spec, &instr);
+    SceneGraphUpdate();
 
-    
+    RunDisplayLoop(comps);
 }
 
 
