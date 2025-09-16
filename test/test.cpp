b0d22c148c8fe5924160742e28b2f5e1022b6a2c
Jakob Garde <jakob@rigsters.com>
2025-09-16 08:02:08 +0200

    have a proper place for the test functions

---
+++ test/test.cpp
@@ -0,0 +1,89 @@
+
+void TestComponentFuncitonsRun() {
+    TimeFunction;
+    printf("TestComponentFuncitonsRun\n\n");
+
+
+    // init
+    MContext *ctx = InitBaselayer();
+    SceneGraphInit();
+    Instrument instr = {};
+
+
+    // NOTE: We must set mcncount BEFORE initialization.
+    //      This is used by API call mcget_ncount(), and called by some components during init (SourceMaxwell)
+    mcncount = 1000000;
+
+
+    // create, configure & init:
+    PSI_DMC spec = {};
+    Init_PSI_DMC(&spec);
+    Array<Component*> comps = Config_PSI_DMC(ctx->a_life, &spec, &instr);
+    SceneGraphUpdate();
+
+
+    // run display & calculate helper matrices:
+    Matrix4f t_world_prev = Matrix4f_Identity();
+    for (s32 i = 0; i < comps.len; ++i) {
+        Component *comp = comps.arr[i];
+        McDisplayNext(ctx->a_pers, comp->transform->t_world);
+
+        DisplayComponent(comp);
+
+        Matrix4f t_world = comp->transform->t_world;
+        comp->t_prev2loc = TransformGetInverse(t_world) * t_world_prev;
+        t_world_prev = t_world;
+
+        ComponentSharedHeader *hdr = comp->GetHeader();
+        hdr-> position_absolute.x =  comp->transform->t_world.m[0][3];
+        hdr-> position_absolute.y =  comp->transform->t_world.m[1][3];
+        hdr-> position_absolute.z =  comp->transform->t_world.m[2][3];
+        hdr-> position_relative.x =  comp->transform->t_loc.m[0][3];
+        hdr-> position_relative.y =  comp->transform->t_loc.m[1][3];
+        hdr-> position_relative.z =  comp->transform->t_loc.m[2][3];
+        for (u32 i = 0; i < 3; i++) {
+            for (u32 j = 0; j < 3; j++) {
+                // NOTE: testing will tell if we may need to mirror the rot matrix, or what
+
+                hdr->rotation_absolute[i][j] = comp->transform->t_world.m[i][j];
+                hdr->rotation_relative[i][j] = comp->transform->t_loc.m[i][j];
+            }
+        }
+    }
+
+
+    // trace
+    u32 DBG_break_after_ncount = 5;
+    for (u32 j = 0; j < mcncount; ++j) {
+        Neutron particle = {};
+
+        for (s32 i = 0; i < comps.len; ++i) {
+            Component *comp = comps.arr[i];
+
+            ParticlePrintWorld(comp->transform->t_world, particle);
+
+            // previous local system -> current local system
+            ParticleTransform(comp->t_prev2loc, &particle);
+
+            // run trace code
+            TraceComponent(comp, &particle, &instr);
+        }
+
+        if (j + 1 == DBG_break_after_ncount) {
+            break;
+        }
+    }
+
+
+    // finally
+    for (s32 i = 0; i < comps.len; ++i) {
+        Component *comp = comps.arr[i];
+
+        FinallyComponent(comp);
+    }
+}
+
+
+void Test() {
+    TestComponentFuncitonsRun();
+}
